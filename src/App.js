98e1f4e82bb0e7cf084ba1bd8276b4c39c247619
Michelle Totherow <tym4me13@gmail.com>
2023-08-29 12:22:07 -0500

    updates to src

--- src/App.js
+++ src/App.js
@@ -11,28 +11,31 @@ import { useState, useEffect } from "react"
 export const App = () => {
 
 const handleBtnClick = () => {
-
+ // what does button do? build 
 }
-  //const [allJokes, setAllJokes ] = useState ([])
-  //const [newJoke, setNewJoke] = useState([])
+  const [allJokes, setAllJokes ] = useState ([])
+  const [setToldJokes, setUntoldJokes] = useState([])
 
   useEffect( () => {
     getAllJokes().then((jokesArray) => {
-      
+      // setAllJokes(jokesArray) here
       console.log("New Jokes Set!")
     })
   }, [])
-// use POST here - 
 
-//   useEffect( () => {
-//     if (newJoke) {
-//       const newJokeList = allJokes.filter(
-//         (joke) => joke.told === false
-//       )
-//       setAllJokes(jokes)
-//     } 
-// }
-//   , [ allJokes])
+  useEffect( () => {
+    if (allJokes) {
+      const toldJokes = allJokes.filter(
+        (joke) => joke.told === false
+      )
+      const notToldJokes = allJokes.filter(
+        (joke) => joke.told === true
+      )
+      setToldJokes(toldJokes)
+      setUntoldJokes(notToldJokes)
+    } 
+}
+  , [allJokes, setToldJokes])
 
   return  <>
   <header className="app-heading"></header>
@@ -44,10 +47,10 @@ const handleBtnClick = () => {
   <input
     className="joke-input"
     type="text"
-    // value={/* What should go here? */}
+    //value={/* What should go here? value of input here */}
     placeholder="New One Liner"
     onChange={(event) => {
-      // What's the value of event?
+      // What's the value of event? set value event.target.value
     }}
   />
   </>
@@ -59,10 +62,8 @@ const handleBtnClick = () => {
 
 // Add a text input to your JSX and capture the user's input. What can be used to store the user's input? How do we capture the user's input?
 
-// Start by adding a jokeService module to your services directory. Add a function here for Posting a new joke.
 
 // Now add a button for posting the new joke. When the user clicks on the button, a new joke with the text the user inputted should be added to the database. All new jokes added to the database should have the told property set to false.
 
 // We want our input field to clear once the joke has been posted. How can we do this? Currently, our input field modifies our state every time it changes, so our state is tied to our input field. Is there a way to tie our input field to our state? Try using that value attribute on the input element.
 
-// complete Coach Steve img
--- src/services/jokeService.js
+++ src/services/jokeService.js
@@ -1,4 +1,8 @@
+// Start by adding a jokeService module to your services directory. Add a function here for Posting a new joke.
+
+
 export const getAllJokes = async () => {
-    
+    // build Post here to add to jokes as string and add key values here
     return fetch(`http://localhost:8088/jokes`).then((res) => res.json())
 }
+

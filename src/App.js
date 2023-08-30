7d8b916bdda7d142d41cd1c3383eaa6f1b5adef5
Michelle Totherow <tym4me13@gmail.com>
2023-08-30 03:14:08 -0500

    update completing mod 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import "./App.css"
-import { getAllJokes } from "./services/jokeService"
+import { getAllJokes, newJokePost } from "./services/jokeService"
 import { useState, useEffect } from "react"
 
-// useEffect to fetch jokes and set to newJokes on initial render
 export const App = () => {
-
-
   const [allJokes, setAllJokes ] = useState ([])
   const [newJoke, setNewJoke] = useState([])
   const [toldJokes, setToldJokes] = useState([])
   const [notToldJokes, setUntoldJokes] = useState([])
 
-  
-
   useEffect( () => {
     getAllJokes().then((jokesArray) => {
       setAllJokes(jokesArray) 
-      console.log("New Jokes Set!")
+      console.log("Jokes Set!")
     })
   }, [])
-// build way for allJokes to display at bottom - like cars and kneels.
 
+// sets empty state
+  useEffect ( () => {
+    setNewJoke("")
+  }, [allJokes])
 
-
+// address logic so that repeated jokes do not add to database
   useEffect( () => {
     if (allJokes) {
       const toldJokes = allJokes.filter(
@@ -34,45 +32,37 @@ export const App = () => {
       setToldJokes(toldJokes)
       setUntoldJokes(notToldJokes)
     } 
-}
-  , [allJokes, setToldJokes, setUntoldJokes])
+  }
+  , [allJokes])
 
-  
-    // what does button do? use postNewJoke here if new, puts in array - if not do this -console.log("Whah, Whah...thats an old one.")
-   
-
-  return  <>
-<div>
-  <h2>Chuckle Checklist</h2>
-  <header className="app-heading"></header>
-</div>
+  return  <div className="app-container">
+  <div >
+    <h1 className="app-heading-text">Chuckle Checklist</h1>
+      <header className="app-heading"></header>
+  </div>
   
   <div className="app-heading-circle">
    <img className="app-logo" src={ require(`./assets/steve.png`)} alt="Good job Steve" />
- </div>
-
-  <button className="joke-add-form" type="text" onClick={(handleBtnClick) => {getAllJokes(newJoke)}}>Joke Me</button>
-  <input
-    className="joke-input"
-    type="text"
-    value={newJoke}
-  //value of input here A form field element should have an id or name attribute
-    placeholder="New One Liner"
-    onChange={(event) => {
-      setNewJoke(event.target.value)
-    }}
-  />
-  {/* <ul>
-    {/* {allJokes.map(joke => {
-      <li key={joke.id}>{joke.text}</li>
-    })}
-  </ul> */} 
-  </>
-
+  </div>
+ 
+  <div className="joke-add-form">
+    <button className="joke-add-form" type="text" onClick={
+      (submitEvent) => {
+      newJokePost(newJoke)}}>Joke Me</button>
+  
+    <input
+      className="joke-input"
+      type="text"
+      value={newJoke}
+      placeholder="New One Liner"
+      onChange={(clickEvent) => {
+        setNewJoke(clickEvent.target.value)
+      }}/>
+  </div>
+    
+  </div>
 }
 
-
-// Add a text input to your JSX and capture the user's input. What can be used to store the user's input? How do we capture the user's input?
 
 
 // Now add a button for posting the new joke. When the user clicks on the button, a new joke with the text the user inputted should be added to the database. All new jokes added to the database should have the told property set to false.
--- src/services/jokeService.js
+++ src/services/jokeService.js
@@ -1,30 +1,33 @@
 // Start by adding a jokeService module to your services directory. Add a function here for Posting a new joke.
+// divide the jobs out into sep func
+// post new jokes with key values
 
 
-export const getAllJokes = async (joke) => {
-    // build Post here to add to jokes as string and add key values here
+export const getAllJokes = async() => {
+    return fetch(`http://localhost:8088/jokes`).then((res) => res.json())
+} // build Post here to add to jokes as string and add key values here
+
+
+export const newJokePost = async(text) => {
+
     const newJoke = {
         id: 0,
         told: false,
-        text: joke
+        text: text
     }
-    const userInput = {
+
+    const res = await fetch (`http://localhost:8088/jokes`
+        ,{
         method: "POST",
         headers: {
                 "Content-Type": "application/json"
         },
         body: JSON.stringify(newJoke)
-    }
+    })
 
-    // postNewJoke into the empty string of "text"  was posting here on every render
-
- const response = await fetch(`http://localhost:8088/jokes`, userInput)
+    await res.json()
+    // 
 }
- 
-
-//await fetch(`http://localhost:8088/jokes`).then((res) => res.json())
-//const response = await fetch("http://localhost:8088/jokes", postNewJoke)
-// 
 
 // export const saveSurveySubmission = async () => {
 //     const postOptions = {
@@ -34,9 +37,7 @@ export const getAllJokes = async (joke) => {
 //         },
 //         body: JSON.stringify(transientState)
 //     }
-
 //     const response = await fetch ("http://localhost:8088/submissions", postOptions)
-
 //     const customEvent = new CustomEvent("newSubmissionCreated")
 //     document.dispatchEvent(customEvent)
 // }
